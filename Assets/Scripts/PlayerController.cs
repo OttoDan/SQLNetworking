a27b0a11ab80c0740f0601dc1cb662a9f7090b32
OttoDan <daniel.f.@live.de>
2018-09-20 00:04:27 +0200

    restructured folders, fixed rotation bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,8 @@ public class PlayerController : MonoBehaviour {
     }
     private void Update()
     {
-        transform.Translate(Input.GetAxis("Vertical") * transform.forward * speed * Time.deltaTime + Input.GetAxis("Horizontal") * transform.right * speed * Time.deltaTime);
+        transform.Translate(Input.GetAxis("Vertical") * transform.forward * speed * Time.deltaTime + Input.GetAxis("Horizontal") * transform.right * speed * Time.deltaTime, Space.World);
+
         if (Input.GetKey(KeyCode.Q))
             transform.Rotate(transform.up, -16 * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.E))
